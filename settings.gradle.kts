1f7a04805412eabe9acbdfb5f7b432a54dfd5950
MrFrydae <MrFrydae@gmail.com>
2023-10-05 09:51:03 -0400

    add auth for scan

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.15"
+}
+
+val isCI = System.getenv("CI") != null
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        isUploadInBackground = !isCI
+        publishAlways()
+
+        capture {
+            isTaskInputFiles = true
+        }
+    }
+}
+
+rootProject.name = "Commands"
+
+include ("example-jda", "fcs-jda", "fcs-core", "fcs-fabric")
+
+project(":fcs-core").name = "fcs-core"
+project(":fcs-jda").name = "fcs-jda"
+project(":fcs-fabric").name = "fcs-fabric"
