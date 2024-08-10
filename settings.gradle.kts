f24cf9a8e60b2f805a4ab625bf6632494a6f218b
MrFrydae <MrFrydae@gmail.com>
2024-08-10 16:06:56 -0400

    Remove Example Projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ gradleEnterprise {
 
 rootProject.name = "Commands"
 
-include ("example-jda", "example-fabric", "fcs-jda", "fcs-core", "fcs-fabric")
+include ("fcs-jda", "fcs-core", "fcs-fabric")
 
 project(":fcs-core").name = "fcs-core"
 project(":fcs-jda").name = "fcs-jda"
